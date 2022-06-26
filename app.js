e9a9cfb7732d1d9d9ee641a2fc9d4b2994f94677
Dbttlnkd <108098172+Dbttlnkd@users.noreply.github.com>
2022-06-26 15:04:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    console.log(e)
 })
 
 document.addEventListener('click', () => {
@@ -10,4 +11,13 @@ document.addEventListener('click', () => {
     setTimeout(() => {
         cursor.classList.remove("expand");
     }, 2000)
+})
+
+const Title = document.querySelector('.underline_intro')
+const bodyScroll = document.querySelector('.main_container')
+
+Title.addEventListener('click', () => {
+    console.log("Ca marche !")
+    bodyScroll.style.transform ="translateY(-100vh)"
+    bodyScroll.style.transition ='1s'
 })
